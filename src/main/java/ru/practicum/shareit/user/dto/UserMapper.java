77f6de3d965dad64c60fcf96d55e20482b9f641e
Alex <MacMorphling@yandex.ru>
2023-06-26 19:43:42 +0300

    fix: запретил наследование мапперам

--- src/main/java/ru/practicum/shareit/user/dto/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserMapper.java
@@ -2,7 +2,7 @@ package ru.practicum.shareit.user.dto;
 
 import ru.practicum.shareit.user.User;
 
-public class UserMapper {
+public final class UserMapper {
     public static UserDto toUserDto(User user) {
         return UserDto.builder()
                 .id(user.getId())
